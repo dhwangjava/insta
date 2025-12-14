e83703efb4300659c878e9acb595422194263c86
dhwangjava <dhwang@dhwa.us>
2025-12-14 13:37:30 -0500

    Update menusifu.js

--- js/menusifu.js
+++ js/menusifu.js
@@ -1,2 +1,2 @@
-var body = JSON.stringify('{   "sessionKey": "b420g7c4235mehk0bqvg2i1ifc",   "sessionKeyRemainingActiveTime": 86399997,   "secretKey": "363aa6df-f071-4dd5-85eb-37391f1b8335",   "result": {     "successful": true   } }')
+var body = JSON.stringify('{   "sessionKey": "66pr29nsaoujti5qt3hb9pimls",   "sessionKeyRemainingActiveTime": 86399997,   "secretKey": "363aa6df-f071-4dd5-85eb-37391f1b8335",   "result": {     "successful": true   } }')
 $done({ body })
